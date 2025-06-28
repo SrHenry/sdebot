e2c82b92d948e2c70b2a5615ea251450dbe0ccff
SrHenry <lucapvh949@gmail.com>
2025-06-28 14:44:07 -0400

    chore: :card_file_box: add data [2025] [1~2bim] [3-ano] [projeto de vida, trilhas de aprofundamento]

---
+++ data/2025/3-ano/conteudos1bim.[trilhas de aprofundamento].js
@@ -0,0 +1,27 @@
+const conteudo = [
+  'Dissertação argumentativa : a redação do Enem - estrutura do formato.',
+  'As competências da redação do Enem.',
+  'As competências da redação do Enem.',
+  'Planejamento da redação do Enem : brainstorming .',
+  'Estrutura do texto dissertativo -argumentativo.',
+  'Construção do parágrafo de introdução do modelo Enem.',
+  'Atividade de produção de texto de introdução do modelo Enem.',
+  'Correção e debate da introdução produzida pelos estudantes.',
+  'Elementos essenciais na introdução do modelo de redação do Enem.',
+];
+
+module.exports = {
+  conteudos: {
+    '3º Ano - Manhã - Trilhas de Aprofundamento - 1º Bimestre': conteudo,
+    '3º Ano - Tarde - Trilhas de Aprofundamento - 1º Bimestre': conteudo,
+  },
+  turmas: {
+    '3º Ano - Manhã - Trilhas de Aprofundamento - 1º Bimestre': [
+      'https://sde.seduc.ro.gov.br/professor/frequencia_busca.php?t=155223&d=175&e=1', //ENSINO MÉDIO REGULAR CH ESTENDIDA - TRILHAS DE APROFUNDAMENTO 3° ANO - M - 5
+    ],
+    '3º Ano - Tarde - Trilhas de Aprofundamento - 1º Bimestre': [
+      'https://sde.seduc.ro.gov.br/professor/frequencia_busca.php?t=155536&d=175&e=1', //ENSINO MÉDIO REGULAR CH ESTENDIDA - TRILHAS DE APROFUNDAMENTO 3° ANO - T - 14
+      'https://sde.seduc.ro.gov.br/professor/frequencia_busca.php?t=155537&d=175&e=1', //ENSINO MÉDIO REGULAR CH ESTENDIDA - TRILHAS DE APROFUNDAMENTO 3° ANO - T - 15
+    ],
+  },
+};
---
+++ data/2025/3-ano/conteudos2bim.[trilhas de aprofundamento].js
@@ -0,0 +1,28 @@
+const conteudo = [
+  'Produção de texto : introdução do modelo Enem.',
+  'Construção do seu parágrafo de desenvolvimento : o D1 - construção do parágrafo ( tópico frasal , sustentação ) .',
+  'Produção do parágrafo de desenvolvimento : o D1.',
+  'Debate sobre a produção do desenvolvimento 1 do parágrafo feito pelos estudantes.',
+  'Contextualização das competências 3 e 4 no desenvolvimento da redação do Enem.',
+  'Atividades de fixação sobre as competências do modelo de redação do Enem.',
+  'A importância da argumentação do modelo Enem de redação.',
+  'Produção da introdução e do desenvolvimento 1 da redação do modelo Enem.',
+  'Debate sobre a produção da introdução e desenvolvimento 1 do modelo Enem.',
+  'Produção do parágrafo de desenvolvimento do modelo Enem de redação.',
+];
+
+module.exports = {
+  conteudos: {
+    '3º Ano - Manhã - Trilhas de Aprofundamento - 2º Bimestre': conteudo,
+    '3º Ano - Tarde - Trilhas de Aprofundamento - 2º Bimestre': conteudo,
+  },
+  turmas: {
+    '3º Ano - Manhã - Trilhas de Aprofundamento - 2º Bimestre': [
+      'https://sde.seduc.ro.gov.br/professor/frequencia_busca.php?t=155223&d=175&e=2', //ENSINO MÉDIO REGULAR CH ESTENDIDA - TRILHAS DE APROFUNDAMENTO 3° ANO - M - 5
+    ],
+    '3º Ano - Tarde - Trilhas de Aprofundamento - 2º Bimestre': [
+      'https://sde.seduc.ro.gov.br/professor/frequencia_busca.php?t=155536&d=175&e=2', //ENSINO MÉDIO REGULAR CH ESTENDIDA - TRILHAS DE APROFUNDAMENTO 3° ANO - T - 14
+      'https://sde.seduc.ro.gov.br/professor/frequencia_busca.php?t=155537&d=175&e=2', //ENSINO MÉDIO REGULAR CH ESTENDIDA - TRILHAS DE APROFUNDAMENTO 3° ANO - T - 15
+    ],
+  },
+};

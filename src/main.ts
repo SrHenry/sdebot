103a26948dbf010cd3a996f0ef838aa8a06baac1
SrHenry <lucapvh949@gmail.com>
2025-06-29 07:20:38 -0400

    feat: :ambulance: add validation to global macros injected through webpack
    There was an issue leading to silent errors and thus breaking the script. Now it's more difficult for it to happen again.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,36 @@
-import { run } from './diario-seduc/functions/run';
-import { runWithQueue } from './diario-seduc/functions/runWithQueue';
+import { debug } from '@/common/functions/debug';
+import { GlobalMacroValidator } from '@/common/validators/GlobalMacroValidator';
+import { parseRawMemory } from '@/diario-seduc/functions/mappers/parseRawMemory';
+import { run } from '@/diario-seduc/functions/run';
+import { runWithQueue } from '@/diario-seduc/functions/runWithQueue';
+
+GlobalMacroValidator.validate({
+  defaultMemory,
+  mode,
+  INTERVAL,
+  CONSUMER_SLEEP_INTERVAL,
+  RATE_LIMIT,
+  CONCURRENCY,
+});
+
+debug('[main::File] >> MACROS validated successfully!');
+debug(
+  `[main::File] >> Running in ${mode} mode with the following configuration:`,
+  {
+    INTERVAL,
+    CONSUMER_SLEEP_INTERVAL,
+    RATE_LIMIT,
+    CONCURRENCY,
+  },
+);
+
+const _memory = parseRawMemory(defaultMemory);
 
 switch (mode) {
   case 'queued':
-    runWithQueue(defaultMemory);
+    runWithQueue(_memory);
     break;
   case 'normal':
-    run(defaultMemory);
+    run(_memory);
     break;
 }
---
+++ src/root.ts
@@ -0,0 +1,4 @@
+import { resolve } from 'node:path';
+export const root = resolve(__dirname, '..');
+
+export default root;

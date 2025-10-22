f68e16500fd954600c9a3ee8503a650dce6fd8b0
SrHenry <lucapvh949@gmail.com>
2025-10-22 01:36:30 -0400

    refactor: :bug: improve `fetchDocument` to handle character encoding correctly When receiving non utf8 encoded HTML, it was corrupting the HTML data

--- src/common/functions/fetchDocument.ts
+++ src/common/functions/fetchDocument.ts
@@ -1,6 +1,24 @@
+export async function fetchDocument(url: string | URL): Promise<Document>;
+
 export async function fetchDocument(url: string | URL): Promise<Document> {
   const response = await fetch(url);
-  const html = await response.text();
+  const parser = new DOMParser();
 
-  return new DOMParser().parseFromString(html, 'text/html');
+  {
+    const contentType = response.headers.get('Content-Type');
+    const charsetMatch = contentType?.match(/charset=([^;]+)/i) ?? null;
+    const encoding = charsetMatch?.[1] ?? null;
+
+    if (encoding === null) {
+      const html = await response.text();
+
+      return parser.parseFromString(html, 'text/html');
+    }
+
+    const decoder = new TextDecoder(encoding);
+    const buffer = await response.arrayBuffer();
+    const html = decoder.decode(buffer);
+
+    return parser.parseFromString(html, 'text/html');
+  }
 }
